ca76edb2eade6fbe087cf7c93a2ae70dd36c90ca
BianXinda <nus.bxd@gmail.com>
2015-12-22 18:45:58 +0800

    1.simplily 2.Add input for pages downloaded

--- 91_crawler/spider91.py
+++ 91_crawler/spider91.py
@@ -1,13 +1,11 @@
 import urllib
 import urllib2
 import re
-from db91 import Db91
-
+import sys
 
 class Spider91:
     def __init__(self, url, count):
         self.db = Db91()
-        self.db.create_db()
         self.entry_url = url
         self.page_count = count
 
@@ -20,10 +18,9 @@ class Spider91:
         print keyid, ' downloaded!'
 
     def query_mp4_url(self, mp4_link_url):
-        pattern = re.compile('http.*mp4')
         request = urllib2.Request(mp4_link_url)
         response = urllib2.urlopen(request)
-        file_url = re.findall(pattern, response.read())
+        file_url = re.findall(r'http.*mp4', response.read())
         if file_url:
             print 'file url:', file_url[0]
             mp4_url = file_url[0]
@@ -32,8 +29,7 @@ class Spider91:
     def query_link_url(self, link_url):
         request = urllib2.Request(link_url)
         response = urllib2.urlopen(request)
-        pattern = re.compile('video_id=.*\' quality')
-        keywords = re.findall(pattern, response.read())
+        keywords = re.findall(r'video_id=.*\' quality', response.read())
         if keywords:
             for key in keywords:
                 keyid = key[9:-16]
@@ -46,9 +42,8 @@ class Spider91:
     def query_page_url(self, page_url):
         request = urllib2.Request(page_url)
         response = urllib2.urlopen(request)
-        pattern = re.compile('<a href="http://www.91porn.com/view_video.php\?viewkey=.*viewtype=basic&category=rf">')
         data = response.read().decode('utf-8')
-        links = re.findall(pattern, data)
+        links = re.findall(r'<a href="http://www.91porn.com/view_video.php\?viewkey=.*viewtype=basic&category=rf">', data)
         for link in links:
             link_url = link[9:-2]
             print 'link:', link_url
@@ -62,7 +57,13 @@ class Spider91:
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print './spider91.py \t\t# default 20 pages \n./spider91.py 100 \t# download 100 pages'
+    if len(sys.argv) == 1:
+        page_count = 20
+    else:
+        page_count = int(sys.argv[1])
+        print '91spider will download videos from page 1 to page ' page_count
     entry_url = 'http://www.91porn.com/video.php?category=rf&page='
-    page_count = 20
     spider = Spider91(entry_url, page_count)
     spider.query_entry_url()
